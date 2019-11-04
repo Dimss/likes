7325e9d4423d13a47f513d8e3d6570987fb35b88
Dmitry Kartsev <dkartsev@redhat.com>
2019-11-04 10:19:09 +0200

    fix env access

--- app/configs/conf.js
+++ app/configs/conf.js
@@ -1,15 +1,14 @@
-
 const devConf = {
-	db: "mongodb://localhost:27017/likes",
-	listenPort: 3001,
-	jwtSecret: 'secret',
+    db: "mongodb://localhost:27017/likes",
+    listenPort: 3001,
+    jwtSecret: 'secret',
 
 };
 
 const prodConf = {
-	db: "mongodb://mongo-likes:27017/likes",
-	listenPort: 8080,
-	jwtSecret: 'secret',
+    db: `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@mongo-likes:27017/${process.env.MONGODB_DATABASE}`,
+    listenPort: 8080,
+    jwtSecret: 'secret',
 };
 exports.conf = devConf;
 if (process.env.PROFILE && process.env.PROFILE === "PROD") exports.conf = prodConf;
